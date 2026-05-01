5e7b676918b0f214ac10083c79ef3d96fd61acf7
Thomas Cruz <salazarthomas738@gmail.com>
2026-05-01 17:03:33 -0500

    Add files via upload

---
+++ nuevo.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+path = r"C:\Users\Thomas\OneDrive\Escritorio\entrega andres\thomas-assessment-main\thomas-assessment\reports"
+
+files = [f for f in os.listdir(path) if f.endswith(".csv")]
+
+print("Archivos encontrados:", files)
+
+
+dfs = []
+for f in files:
+    file_path = os.path.join(path, f)
+    print(f"Leyendo: {file_path}")
+    df = pd.read_csv(file_path)
+    dfs.append(df)
+
+
+datos = pd.concat(dfs, ignore_index=True)
+
+antes = len(datos)
+datos = datos.drop_duplicates()
+despues = len(datos)
+
+print(f"Duplicados eliminados: {antes - despues}")
+
+
+print(os.listdir(path))
+print(datos)
+
